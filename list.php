c54e633f2b9dff9bb94f0e330b85c0f9517ad160
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 12:13:46 +0700

    Create file list.php

---
+++ list.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <!-- CSS login -->
+    <link rel="stylesheet" href="css/list.css">
+
+    <title>List Rumah Sakit Rujukan</title>
+</head>
+
+<body>
+    <div class="container border rounded mt-3">
+        <div class="row mt-2">
+            <h1 class="text-center">List Rumah Sakit Rujukan</h1>
+        </div>
+
+        <!-- Table -->
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- End of table -->
+    </div>
+
+
+    <!-- Javascript -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Javascript -->
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- jS login -->
+    <script src=""></script>
+    <!-- End of javascript -->
+
+</body>
+
+</html>
