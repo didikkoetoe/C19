5277a06a762e24d198223b90b12d6725401c1141
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-24 15:44:39 +0700

    Edit name function and add insert funtion

--- functions/functions.php
+++ functions/functions.php
@@ -9,14 +9,14 @@ function create($post)
 
     // take value
     $nik = htmlspecialchars($post["nik"]);
-    $name = htmlspecialchars($post["name"]);
+    $nama = htmlspecialchars($post["nama"]);
     $birthday = $post["birthday"];
-    $number = htmlspecialchars($post["number"]);
-    $address = htmlspecialchars($post["address"]);
+    $nomor = htmlspecialchars($post["nomor"]);
+    $alamat = htmlspecialchars($post["alamat"]);
     $ke = htmlspecialchars($post["ke"]);
 
     $query = "INSERT INTO vaksin VALUES (
-        '', '$nik', '$name' , '$birthday', '$number', '$address', '$ke')";
+        '', '$nik', '$nama' , '$birthday', '$nomor', '$alamat', '$ke')";
 
     mysqli_query($conn, $query);
 
@@ -39,6 +39,32 @@ function delete($id)
     global $conn;
 
     mysqli_query($conn, "DELETE FROM vaksin WHERE id=$id");
+
+    return mysqli_affected_rows($conn);
+}
+
+function update($post)
+{
+    global $conn;
+
+    $id = $post["id"];
+    $nik = htmlspecialchars($post["nik"]);
+    $nama = htmlspecialchars($post["nama"]);
+    $birthday = $post["birthday"];
+    $nomor = htmlspecialchars($post["nomor"]);
+    $alamat = htmlspecialchars($post["alamat"]);
+    $ke = htmlspecialchars($post["ke"]);
+
+    $query = "UPDATE vaksin SET
+            nik = '$nik',
+            nama = '$nama',
+            birthday = '$birthday',
+            nomor = '$nomor',
+            alamat = '$alamat',
+            ke = '$ke'
+        WHERE id=$id";
+
+    mysqli_query($conn, $query);
 
     return mysqli_affected_rows($conn);
 }
